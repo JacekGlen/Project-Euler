b99d0562ba8b4bd6776cfcbc7c5da77aa5226abf
Jacek Glen <jacek.glen@gmail.com>
2016-04-26 00:37:01 +0100

    Solution to Problem 5

---
+++ Simple/Problems/Problem5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.Problems
+{
+    public class Problem5
+    {
+        public int CalculateSmallestMultiple(int upper)
+        {
+            int[] multipliers = new int[upper + 1];
+
+            for (int n = 2; n <= upper; ++n)
+            {
+                int i = 2;
+                int count = 0;
+                int current = n;
+
+                while (i <= current)
+                {
+                    if (current % i == 0)
+                    {
+                        current = current / i;
+                        ++count;
+                        multipliers[i] = Math.Max(count, multipliers[i]);
+                    }
+                    else
+                    {
+                        ++i;
+                        count = 0;
+                    }
+                }
+            }
+
+            var multiple = multipliers.Select((a, idx) => Math.Pow(idx, a)).Aggregate((x,y) => x * y);
+
+            return Convert.ToInt32(multiple);
+        }
+    }
+}
---
+++ SimpleTests/Problem5Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Simple.Problems;
+
+namespace SimpleTests
+{
+    [TestFixture]
+    public class Problem5Test
+    {
+        [Test]
+        [TestCase(2,2)]
+        [TestCase(3, 6)]
+        [TestCase(4, 12)]
+        [TestCase(10, 2520)]
+        [TestCase(20, 0)]
+        public void ReturnsCorrectValueForValidParameter(int upper, int expected)
+        {
+            var sut = new Problem5();
+
+            var result = sut.CalculateSmallestMultiple(upper);
+
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
