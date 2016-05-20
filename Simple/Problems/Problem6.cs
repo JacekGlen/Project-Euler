a78d04d39620b4a86a17c05227982ce93572ff56
Jacek Glen <jacek.glen@gmail.com>
2016-05-20 21:08:42 +0100

    Add solution for Problem 6

--- Simple/Problems/Problem6.cs
+++ Simple/Problems/Problem6.cs
@@ -8,6 +8,26 @@ namespace Simple.Problems
 {
     public class Problem6
     {
+        public long GetDifference(int n)
+        {
+            var sumSquares = GetSumOfSquares(n);
+            var squareSum = GetSquareOfSum(n);
 
+            return squareSum - sumSquares;
+        }
+
+        private long GetSquareOfSum(int n)
+        {
+            var sum = (n * (n + 1)) / 2;
+
+            return Convert.ToInt64( Math.Pow(sum, 2));
+        }
+
+        private long GetSumOfSquares(int n)
+        {
+            var sum = (n * (n + 1) * (2 * n + 1)) / 6;
+
+            return sum;
+        }
     }
 }
--- SimpleTests/Problem6Test.cs
+++ SimpleTests/Problem6Test.cs
@@ -12,5 +12,16 @@ namespace SimpleTests
     [TestFixture]
     public class Problem6Test
     {
+        [TestCase(10, 2640)]
+        [TestCase(100, 2640)]
+        public void CalculatesDifference(int n, long expectedResult)
+        {
+            var sut = new Problem6();
+
+            var result = sut.GetDifference(n);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
     }
 }
